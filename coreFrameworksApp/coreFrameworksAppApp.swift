9f2a53c412082e3d9d31f37b02231918fbaf2d7f
keyframe007 <keyframe007@gmail.com>
2023-10-07 17:20:05 -0400

    Initial Commit

---
+++ coreFrameworksApp/coreFrameworksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  coreFrameworksAppApp.swift
+//  coreFrameworksApp
+//
+//  Created by Ali Tezel on 2023-10-07.
+//
+
+import SwiftUI
+
+@main
+struct coreFrameworksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
