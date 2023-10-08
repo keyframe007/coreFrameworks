c6aca84ffffff3257ad7f92ffd2c33983be9ff90
keyframe007 <keyframe007@gmail.com>
2023-10-07 21:14:11 -0400

    grid basic

--- coreFrameworksApp/FrameworkGridView.swift
+++ coreFrameworksApp/FrameworkGridView.swift
@@ -8,25 +8,62 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())
+    ]
        
     var body: some View {
-        
-        
-        VStack {
-            Image("app-clip")
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text("App Clips")
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.6)
+        NavigationView{
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+            }
             
-            
+                }
+                .navigationTitle("Apple Frameworks")
+            }
         }
     }
 }
+        
 
 #Preview {
     FrameworkGridView()
 }
+            
+            
+// STRUCTS BELOW THIS LINE
+
+struct FrameworkTitleView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
+        
+        
+        
+        
+            
+
+
+
+
+        
